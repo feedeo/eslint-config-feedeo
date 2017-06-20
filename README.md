e8f07845d510f72cbf17fa4c214960c5fab7ad27
Hugo Freire <hfreire@exec.sh>
2017-06-20 17:32:41 +0200

    chore: copied code from eslint-config-hfreire

---
+++ README.md
@@ -0,0 +1,14 @@
+# Feedeo's ESLint configuration
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+[![Build Status](https://travis-ci.org/feedeo/eslint-config-feedeo.svg?branch=master)](https://travis-ci.org/feedeo/eslint-config-feedeo)
+[![Coverage Status](https://coveralls.io/repos/github/feedeo/eslint-config-feedeo/badge.svg?branch=master)](https://coveralls.io/github/feedeo/eslint-config-feedeo?branch=master)
+[![](https://img.shields.io/github/release/feedeo/eslint-config-feedeo.svg)](https://github.com/feedeo/eslint-config-feedeo/releases)
+[![](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+[![Version](https://img.shields.io/npm/v/@feedeo/eslint-config-feedeo.svg)](https://www.npmjs.com/package/@feedeo/eslint-config-feedeo)
+[![Downloads](https://img.shields.io/npm/dt/@feedeo/eslint-config-feedeo.svg)](https://www.npmjs.com/package/@feedeo/eslint-config-feedeo) 
+
+### How to install
+```
+npm install @feedeo/eslint-config-feedeo
+```
---
+++ src/eslint-config-feedeo.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017, Feedeo AB <hugo@feedeo.io>.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  extends: [
+    'standard'
+  ],
+  env: {
+    node: true
+  },
+  plugins: [
+    'json',
+    'mocha',
+    'promise',
+    'standard',
+    'unicorn'
+  ],
+  rules: {
+    'promise/always-return': 'off',
+    'promise/no-return-wrap': 'error',
+    'promise/param-names': 'error',
+    'promise/catch-or-return': 'error',
+    'promise/no-native': 'error',
+    'promise/no-callback-in-promise': 'error',
+    'mocha/no-exclusive-tests': 'error',
+    'mocha/no-global-tests': 'error',
+    'mocha/no-identical-title': 'error',
+    'unicorn/filename-case': [ 'error', { 'case': 'kebabCase' } ],
+    'unicorn/no-abusive-eslint-disable': 'error',
+    'unicorn/no-process-exit': 'error',
+    'unicorn/no-new-buffer': 'error'
+  },
+  globals: {
+    'describe': true,
+    'it': true,
+    'before': true,
+    'beforeEach': true,
+    'after': true,
+    'afterEach': true,
+    'td': true,
+    '_': true,
+    'should': true
+  }
+}
